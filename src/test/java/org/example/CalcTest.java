b4ed9b9552eef1875fb2bac6794392c1b1232d88
yangjiaqi01 <483248575@qq.com>
2024-10-03 23:17:11 +0800

    Fix subtract method

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -2,7 +2,14 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
+class Calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
 public class CalcTest {
   Calc c = new Calc();
 
@@ -12,7 +19,7 @@ public class CalcTest {
     }
 
     @Test
-     void testSubtraction() {
+      void testSubtraction() {
         assertEquals(2, c.subtract(4, 2));
     }
 
